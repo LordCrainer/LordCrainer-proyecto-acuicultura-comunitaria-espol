87c871bc783e37428edcf70edadb2f90bc5a8a3f
LordCrainer <camogan3000@hotmail.com>
2020-08-29 10:33:56 -0500

    Prueba del server api-rest

---
+++ README.md
@@ -0,0 +1,6 @@
+EJEMPLOS USANDO ESP8266
+https://github.com/luisllamasbinaburo/ESP8266-Examples/tree/master/22_API_REST_Server
+
+LIBRERÍAS
+https://github.com/me-no-dev/ESPAsyncWebServer
+https://github.com/me-no-dev/ESPAsyncTCP
---
+++ arduino/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "No Molestar_5G";
+const char* password = "Carlosolrac306011";
+const char* hostname = "ESP8266_1";
+
+IPAddress ip(192, 168, 150, 75);
+IPAddress gateway(192, 168, 150, 1);
+IPAddress subnet(255, 255, 255, 0);
