897e450bf89cb5853039c5c82c064afef9250aa8
LordCrainer <camogan3000@hotmail.com>
2020-08-31 16:16:20 -0500

    api-rest: Restaurando la variable puerto y mejorando el archivo env.example.h

--- arduino/env.example.h
+++ arduino/env.example.h
@@ -1,7 +1,13 @@
+// CONFIGURACION DEL SERVIDOR
+const PROGMEM  uint16_t port = 80;
+
+// CONFIGURACION DEL SERVER
+
+const PROGMEM  uint16_t port = 80;
+
 // CONFIGURACION DE LA RED
 
 const char* ssid     = "";
 const char* password = "";
 const char* hostname = "ESP8266_1";
 
-const PROGMEM  uint16_t port = 80;
