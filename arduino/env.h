216d3e51d9bd990ba1f4b10931ee94473f1b500b
Crainer <camogan3000@hotmail.com>
2020-10-12 17:26:03 -0500

    Update env.h

--- arduino/env.h
+++ arduino/env.h
@@ -1,7 +1,7 @@
 // CONFIGURACION DE LA RED
 
-const char* ssid     = "No Molestar_5.1G";
-const char* password = "carlosjuan3017";
+const char* ssid     = "";
+const char* password = "";
 const char* hostname = "ESP8266_1";
 
 const PROGMEM  uint16_t port = 80;
