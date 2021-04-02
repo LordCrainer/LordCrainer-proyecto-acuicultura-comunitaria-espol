c39e01cb9ef816be91a58158078d5ee17e37ee90
LordCrainer <camogan3000@hotmail.com>
2021-04-02 12:34:33 -0500

    device: Agregando los datos a la estructura IMeasurement, luego pasarlo a json

--- arduino/api/components/device/2.application/start.hpp
+++ arduino/api/components/device/2.application/start.hpp
@@ -1,12 +1,23 @@
 String startDevice()
 {
     // Obtener el dato de la piscina a medir pool_id
+    IMeasurement data;
+    data.device_id = 1;
+    
     String pool = "1";
     // Capturar el tiempo actual: 2021-03-01-13-01
     String time = "20210301-1301";
     String filename = "P" + pool + "_" + time + ".json"; // P1_20210301-1301.json
     // Leer los datos de los sensores
-    // const byte temp = getTemp();
+    // getTemp();
+    IParams temp;
+    temp.name = "Temperature";
+    temp.value = 10.0;
+    // getPh();
+    IParams ph;
+    ph.value = 7.0;
+    ph.name = "PH";
+
     // Verificar si existe un archivo parecido
     // const existFile = verifyFile(filename);
     const byte existFile = 1;
