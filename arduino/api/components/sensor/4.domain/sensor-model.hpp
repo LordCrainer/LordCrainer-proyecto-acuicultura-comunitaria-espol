deff3d1a3c6cde576888f44122056e88a593bfd0
LordCrainer <camogan3000@hotmail.com>
2021-04-03 02:11:42 -0500

    sensor: cambio de nombre y probando el model general

--- arduino/api/components/sensor/4.domain/sensor-model.hpp
+++ arduino/api/components/sensor/4.domain/sensor-model.hpp
@@ -2,21 +2,23 @@ String sensorModel(IParams data)
 {
     String json;
     StaticJsonDocument<50> doc;
+    JsonObject obj = doc.as<JsonObject>();
     doc["value"] = data.value;
     doc["name"] = data.name;
     serializeJson(doc, json);
     return json;
 }
 
-/* String modelAllSensor(IParams data1, IParams data2)
+String modelAllSensor(IParams data1, IParams data2)
 {
     String json;
     StaticJsonDocument<150> doc;
     String Temp = sensorModel(data1);
     String PH = sensorModel(data2);
-    JsonDocument arr = doc.as<JsonArray>();
-    arr.add(Temp);
-    arr.add(PH);
+    JsonDocument arr = doc.createNestedArray("params");
+    arr[0] = Temp;
+    //arr.add(Temp);
+    //arr.add(PH);
     serializeJson(doc, json);
     return json;
-} */
+}
